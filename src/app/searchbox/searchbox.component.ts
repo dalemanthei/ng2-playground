7a29a9145f44ebb91bc34fa3af684f5e4fb8a350
Dale Manthei <dalemanthei@users.noreply.github.com>
2016-11-20 10:44:08 -0600

    Change project to use sass by default

--- src/app/searchbox/searchbox.component.ts
+++ src/app/searchbox/searchbox.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-searchbox',
   templateUrl: './searchbox.component.html',
-  styleUrls: ['./searchbox.component.css']
+  styleUrls: ['./searchbox.component.scss']
 })
 export class SearchboxComponent implements OnInit {
 
